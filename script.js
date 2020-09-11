20b77baf32fb1e891855bbd4945b03ec82681f74
Greengrer <51015697+Greengrer@users.noreply.github.com>
2020-09-12 00:22:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Lynn"&&b=="is awesome")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
